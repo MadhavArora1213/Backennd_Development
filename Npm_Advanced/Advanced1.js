4e3eded89a1ea729102e8af005a0dfbcbe86f253
Madhav Arora <madhavarora132005@gmail.com>
2025-01-29 20:58:11 +0530

    day3

---
+++ Npm_Advanced/Advanced1.js
@@ -0,0 +1,13 @@
+// NPM understanding
+
+// nodejs ke core me jo installed ata hai wo kehlaata hai modules.
+
+// npm se downlaod krte hai wo hota hai packages.
+
+// dependencies --> packages and packages ke dependencies
+
+// devDependencies --> aise packages jo sirf development ke time pe use hote hai.or jab ham use deploy krde ge to hum usse use nhi kr rehe honge
+
+// npm start or test it is installed when download node and npm  it is in our operating system.
+
+// npm chacha when custom command is there  we use run  scripts npm run chacha.
